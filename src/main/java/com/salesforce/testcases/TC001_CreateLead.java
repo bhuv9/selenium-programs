f4349ac1de8436f018287aaf64054426ae29a9eb
DELL <DELL@DESKTOP-EI8VAKO>
2025-08-19 19:12:41 +0530

    --AugustUpdate--

---
+++ src/main/java/com/salesforce/testcases/TC001_CreateLead.java
@@ -0,0 +1,43 @@
+package com.salesforce.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectSpecificMethods;
+import com.salesforce.pages.LoginPage;
+
+public class TC001_CreateLead  extends ProjectSpecificMethods{
+	
+	
+	@BeforeTest
+	public void setValues() {
+		excelFileName="CreateLead";
+		testcaseName="Salesforce Application";
+		testDescription="Login page";
+		authors="Bhuvanesh";
+		category="Sanity";
+	}
+	
+	
+	@Test(dataProvider = "fetchData")
+	public void createLead(String uName,String passWord,String firstName,String lastName,String cName) {
+		
+		LoginPage lp=new LoginPage();
+		lp.enterUserName(uName)
+		.enterPassword(passWord)
+		.clickOnLogin()
+		.clickOnAppLauncher()
+		.clickOnViewAll()
+		.clickOnSales()
+		.clickLeadsTab()
+		.clickNewLeadsButton()
+		.selectsalutationDropdown()
+		.enterFirstName(firstName)
+		.enterLastName(lastName)
+		.enterCompanyName(cName)
+		.clickSaveLead();
+		
+
+	}
+
+}
